937a8ebe2573896fed43fa92ed825cafc53d5fc1
orsospatrick <orsospatrick@yahoo.com>
2019-11-13 20:44:55 +0200

    fsfsf

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -24,11 +24,7 @@
 	/* Navbar Scripts */
 	// jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
+	
     });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
