09e56b08716d71c0f0335fd2e8a1ce66787f2f8f
orsospatrick <orsospatrick@yahoo.com>
2019-11-13 21:01:03 +0200

    sfsf

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -24,25 +24,16 @@
 	/* Navbar Scripts */
 	// jQuery to collapse the navbar on scroll
     $(window).on('scroll load', function() {
-        if ($(".navbar").offset().top < 400) {
-            $(".navbar").fadeIn();
+        if ($(".navbar").offset().top > 20) {
+            $(".fixed-top").addClass("top-nav-collapse");
+        } else {
+            $(".fixed-top").removeClass("top-nav-collapse");
         }
-        else {
-            $(".navbar").fadeOut();
-               }
 
     });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	$(function() {
-		$(document).on('click', 'a.page-scroll', function(event) {
-			var $anchor = $(this);
-			$('html, body').stop().animate({
-				scrollTop: $($anchor.attr('href')).offset().top
-			}, 600, 'easeInOutExpo');
-			event.preventDefault();
-		});
-	});
+	
 
     // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
