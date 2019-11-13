48428cf4f2036ca19cc7059b3431a1cca08e629c
orsospatrick <orsospatrick@yahoo.com>
2019-11-13 21:03:54 +0200

    fsfsfs

--- web/js/scripts.js
+++ web/js/scripts.js
@@ -33,7 +33,15 @@
     });
 
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
-	
+	$(function() {
+		$(document).on('click', 'a.page-scroll', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 600, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
 
     // closes the responsive menu on menu item click
     $(".navbar-nav li a").on("click", function(event) {
